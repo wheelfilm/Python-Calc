525c90cf00b0c279f54e62a18314309b068ac7ed
wheelfilm <austnwheel@gmail.com>
2015-03-18 23:14:59 -0400

    Completed the basic program for Python-Calc

--- Calculator.py
+++ Calculator.py
@@ -1,25 +1,46 @@
-#Python Test!!!
+#----------------------------
+# Python Test Calculator!!! -
+# Created by Austin Wheeler -
+# austnwheel@gmail.com      -
+#----------------------------
 
-#Input the first number and make sure it is an integer.
-print("Type your first number here:")
-def number_1:
-    num1 = input()
-    if num1 is not int:
-        print("Try again!")
-    else:
-        print(
+def add(x, y):
+    """Add the two given values!"""
+    return x + y
+def subtract(x, y):
+    """Subtract the two given values!"""
+    return x - y
+def multiply(x, y):
+    """Multiply the two given values!"""
+    return x * y
+def divide(x, y):
+    """Divide the two given values!"""
+    return x / y
 
-#Input the operator and make sure it is an integer.
-print("Type your operator here(+,-,*,/):")
-def operator():
-    if input() == "+":
-        op = input()
-    elif input() == "-":
-        op = input()
-    elif input() == "*":
-        op = "*"
-    elif input() == "/":
-        op = "/"
-    else:
-        print("You didn't input an operator! Try again!")
-    return False
+print("Input your first number:")
+x = int(input())
+
+print("Input the number for your desired operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+op = str(input("1/2/3/4: "))
+
+print("Input your second number:")
+y = int(input())
+
+if op == '1':
+    result = add(x, y)
+    print x, "plus", y, "=", result
+elif op == '2':
+    result = subtract(x, y)
+    print x, "minus", y, "=", result
+elif op == '3':
+    result = multiply(x, y)
+    print x, "times", y, "=", result
+elif op == '4':
+    result = divide(x, y)
+    print x, "divided by", y, "=", result
+
+print("This program was a success!")
