e7e5857d4306c19183d5bfcb1f0b5b22d2a63726
wheelfilm <austnwheel@gmail.com>
2015-03-18 21:13:52 -0400

    Add basic code previously worked on to start from.

---
+++ Calculator.py
@@ -0,0 +1,25 @@
+#Python Test!!!
+
+#Input the first number and make sure it is an integer.
+print("Type your first number here:")
+def number_1:
+    num1 = input()
+    if num1 is not int:
+        print("Try again!")
+    else:
+        print(
+
+#Input the operator and make sure it is an integer.
+print("Type your operator here(+,-,*,/):")
+def operator():
+    if input() == "+":
+        op = input()
+    elif input() == "-":
+        op = input()
+    elif input() == "*":
+        op = "*"
+    elif input() == "/":
+        op = "/"
+    else:
+        print("You didn't input an operator! Try again!")
+    return False
